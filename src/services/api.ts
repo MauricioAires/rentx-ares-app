7bb8755c2d13fa9a11867b020476877394419f2e
MauricioAires <mauricioaires015@gmail.com>
2024-02-19 15:45:20 -0300

    feat(services) axios

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+function sleep(ms = 2000): Promise<void> {
+  console.log("Kindly remember to remove `sleep`");
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export const api = axios.create({
+  baseURL: `http://192.168.1.7:3333`,
+  headers: { accept: "application/json" },
+});
+
+api.interceptors.response.use(async (response) => {
+  // add artificial delay for dev env
+  if (process.env.NODE_ENV === "development") {
+    await sleep();
+  }
+  return response.data;
+});
