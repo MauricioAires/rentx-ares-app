4e93f858278ee5bd36994d4adbca2ea7565a7028
MauricioAires <mauricioaires015@gmail.com>
2024-02-22 10:32:37 -0300

    feat(components) create slider without deps

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,27 +1,48 @@
+import React, { useState, useRef } from "react";
+import { FlatList, ViewToken } from "react-native";
+
 import * as S from "./styles";
 
 interface ImageSliderProps {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: ImageSliderProps) {
+  const [imageVisibleIndex, setImageVisibleIndex] = useState(0);
+
+  const handleChangeImageVisible = useRef((info: ChangeImageProps) => {
+    setImageVisibleIndex(info.viewableItems[0].index!);
+  });
   return (
     <S.Container>
       <S.ImageIndexes>
-        <S.ImageIndex active={true} />
-        <S.ImageIndex active={false} />
-        <S.ImageIndex active={false} />
-        <S.ImageIndex active={false} />
+        {imagesUrl.map((_, index) => (
+          <S.ImageIndex key={index} active={index === imageVisibleIndex} />
+        ))}
       </S.ImageIndexes>
 
-      <S.CarImageWrapper>
-        <S.CarImage
-          source={{
-            uri: imagesUrl[0],
-          }}
-          resizeMode="contain"
-        />
-      </S.CarImageWrapper>
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={(key) => key}
+        renderItem={({ item }) => (
+          <S.CarImageWrapper>
+            <S.CarImage
+              source={{
+                uri: item,
+              }}
+              resizeMode="contain"
+            />
+          </S.CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={handleChangeImageVisible.current}
+      />
     </S.Container>
   );
 }
